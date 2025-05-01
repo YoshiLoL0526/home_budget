436c3cc264e25c7d175dd554f50b3a37cdfff14b
Raidel <yoshilol0526@gmail.com>
2025-05-01 15:47:46 -0300

    Updated database require SSL

--- home_budget/settings.py
+++ home_budget/settings.py
@@ -99,6 +99,9 @@ else:
             "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
             "HOST": os.environ.get("DB_HOST", "localhost"),
             "PORT": os.environ.get("DB_PORT", "5432"),
+            "OPTIONS": {
+                "sslmode": "require",
+            },
         }
     }
 
